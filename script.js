f88fd37f69902105d780d58f253951161dc706e2
garrettmichaelgeorge <garrett.michael.george@gmail.com>
2021-04-20 19:47:50 -0400

    Complete Zip feature with styling

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const inputEl = document.getElementById("input-zip-code")
 const formEl = document.getElementById("form-zip-code")
 const target = document.getElementById("target")
 const template = document.getElementById("template")
+const body = document.body
 
 formEl.addEventListener("submit", event => {
   event.preventDefault()
@@ -10,33 +11,82 @@ formEl.addEventListener("submit", event => {
   const zip = inputEl.value
   const url = `https://api.zippopotam.us/us/${zip}`
 
-  fetch(url)
-    .then(response => {
-      response.json()
-        .then(data => {
-          console.log(data)
-          processData(data)
-        })
-    })
+  fetchDataFrom(url)
 })
 
-function processData(data) {
-  data.places.forEach(place => processPlace(place))
+function fetchDataFrom(url) {
+    fetch(url)
+      .then(response => {
+        if (!response.ok) throw response
+
+        response.json()
+          .then(data => {
+            const zip = new Zip(data)
+            zip.render()
+          })
+      })
+    .catch(_error => {
+      alert("Invalid zip code!")
+    })
 }
 
-function processPlace(place) {
-  const placeKeys = Object.keys(place)
+class Zip {
+  constructor (data) {
+    this.data = data
+  }
 
-  placeKeys.forEach(key => {
-    const el = cloneTemplate(template)
+  get places () {
+    return this.data.places
+  }
 
-    el.firstElementChild.textContent = key
-    el.lastElementChild.textContent = place[key]
+  get place () {
+    return this.places[0]
+  }
 
-    target.appendChild(el)
-  })
+  get stateAbbreviation () {
+    return this.place["state abbreviation"]
+  }
+
+  render () {
+    target.innerHTML = ""
+
+    this.places.forEach(place => {
+      this._renderPlace(place)
+    })
+
+    requestAnimationFrame(() => {
+      body.classList.toggle("dark-mode")
+      body.classList.toggle("light-mode")
+    })
+  }
+
+  _renderPlace (place) {
+    const placeKeys = Object.keys(place)
+
+    placeKeys.forEach(key => {
+      const el = cloneTemplate(template)
+
+      el.firstElementChild.textContent = this._formatKey(key)
+      el.lastElementChild.textContent = this._formatInfo(place[key])
+
+      target.appendChild(el)
+    })
+  }
+
+  _formatKey (str) {
+    return `${capitalize(str)}: `
+  }
+
+  _formatInfo (str) {
+    return str
+  }
 }
 
+// Utility
 function cloneTemplate(template) {
   return template.content.cloneNode(true)
+}
+
+function capitalize(str) {
+  return str[0].toUpperCase() + str.slice(1)
 }
